b84dc266240264476baff0d7a109dd2cfb1281a6
Brandon Kim <bkim377@gatech.edu>
2020-05-04 21:15:16 -0400

    started styling the navbar

--- react-portfolio/src/components/NavTabs.js
+++ react-portfolio/src/components/NavTabs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./NavTabs.css"
 
 function NavTabs() {
   // We'll go into the Hooks API later, for now, we are just using some code
@@ -8,29 +9,22 @@ function NavTabs() {
   const location = useLocation();
 
   return (
-    <ul className="nav nav-tabs">
+    <nav className="navbar navbar-expand-lg border-bottom border-light">
+            <h1><span className="navbar-brand mb-3 ml-3">Brandon's Professional Profile</span></h1>
+            <div className="navbar-collapse" id="navbarSupportedContent">
+    <ul className="nav navbar-nav nav-flex-icons ml-auto">
       <li className="nav-item">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Link>
       </li>
       <li className="nav-item">
-        <Link
-          to="/about"
-          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-        >
-          About
-        </Link>
+        <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>About</Link>
       </li>
       <li className="nav-item">
-        <Link
-          to="/contact"
-          className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-        >
-          Contact
-        </Link>
+        <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
       </li>
     </ul>
+    </div>
+    </nav>
   );
 }
 
