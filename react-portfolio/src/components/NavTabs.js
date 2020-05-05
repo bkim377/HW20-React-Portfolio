bbb8117d130f10ba819db277c181c63f994911f8
Brandon Kim <bkim377@gatech.edu>
2020-05-05 17:14:50 -0400

    npm installed FontAwesome to add navbar icons

--- react-portfolio/src/components/NavTabs.js
+++ react-portfolio/src/components/NavTabs.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faComment, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import "./NavTabs.css"
 
 function NavTabs() {
@@ -14,13 +16,13 @@ function NavTabs() {
             <div className="navbar-collapse" id="navbarSupportedContent">
     <ul className="nav navbar-nav nav-flex-icons ml-auto">
       <li className="nav-item">
-        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>About</Link>
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}><FontAwesomeIcon icon={faUser} /> About</Link>
       </li>
       <li className="nav-item">
-        <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</Link>
+        <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}><FontAwesomeIcon icon={faComment} /> Portfolio</Link>
       </li>
       <li className="nav-item">
-        <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
+        <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}><FontAwesomeIcon icon={faEnvelope} /> Contact</Link>
       </li>
     </ul>
     </div>
--- react-portfolio/src/components/pages/About.js
+++ react-portfolio/src/components/pages/About.js
@@ -11,7 +11,7 @@ function About() {
                     </div>
                 <div className="row p-3">
                   <div className="col-sm-8">
-                    <img src = "./assets/imgs/BrandonKim.png" alt="Profile Picture"></img>
+                    <img src = "./assets/imgs/BrandonKim.png" alt="Profile Picture" />
                     <h3>About Me</h3>
                     <p>Graduated from Georgia Tech in December 2019 with a Bachelor of Science in Biomedical Engineering. 
                         Planning to enroll in graduate school at Georgia Tech's MS-HCI (Masters in Human-Computer Interaction) program in August 2020.
@@ -21,13 +21,13 @@ function About() {
                     </div>
                     <div className="col-sm-4">
                     <h4>My Current Work:</h4>
-                    <img className="social-media" src="./assets/imgs/github.png" alt="GitHub"></img>
+                    <img className="social-media" src="./assets/imgs/github.png" alt="GitHub" />
                     <a href="https://github.com/bkim377">GitHub</a>
                     <p></p>
-                    <img className="social-media" src="./assets/imgs/linkedin.png" alt="LinkedIn"></img>
+                    <img className="social-media" src="./assets/imgs/linkedin.png" alt="LinkedIn" />
                     <a href="https://www.linkedin.com/in/brandongkim/">LinkedIn</a>
                     <p></p>
-                    <img className="social-media" src="./assets/imgs/resume.png" alt="Resume"></img>
+                    <img className="social-media" src="./assets/imgs/resume.png" alt="Resume" />
                     <a href="https://drive.google.com/open?id=1al1y7Y_mezcPjcaxnSP56YcNSw9KCOOh">My Resume</a>
                     </div>
                 </div>
